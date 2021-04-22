752f312d28ad775c6b30ad8d0834ce1088c0e067
brianlball <brian.ball@nrel.gov>
2021-04-21 19:01:57 -0600

    add thermal example

--- URBANopt_REopt/lib/urbanopt/mappers/ThermalStorage.rb
+++ URBANopt_REopt/lib/urbanopt/mappers/ThermalStorage.rb
@@ -56,84 +56,23 @@ module URBANopt
         # Add ice to applicable TES object building and set applicable charge and discharge times
 
         if feature_names[0].to_s == 'Mixed_use 1'
-          OpenStudio::Extension.set_measure_argument(osw,
-                                                     'add_central_ice_storage', '__SKIP__', false)
+          OpenStudio::Extension.set_measure_argument(osw, 'add_central_ice_storage', '__SKIP__', false)
           OpenStudio::Extension.set_measure_argument(osw, 'add_central_ice_storage', 'storage_capacity', 6000)
+          OpenStudio::Extension.set_measure_argument(osw, 'add_central_ice_storage', 'chiller_resize_factor', 1.0)
+          OpenStudio::Extension.set_measure_argument(osw, 'add_central_ice_storage', 'chiller_limit', 1.0)
+          OpenStudio::Extension.set_measure_argument(osw, 'add_central_ice_storage', 'loop_sp', 44.0)
+          OpenStudio::Extension.set_measure_argument(osw, 'add_central_ice_storage', 'inter_sp', 47.0)
+          OpenStudio::Extension.set_measure_argument(osw, 'add_central_ice_storage', 'chg_sp', 25.0)
+          OpenStudio::Extension.set_measure_argument(osw, 'add_central_ice_storage', 'dr_dur', 3.0)
         end
 
         if feature_names[0].to_s == 'Restaurant 1'
-          OpenStudio::Extension.set_measure_argument(osw,
-                                                     'add_packaged_ice_storage', '__SKIP__', false)
-        end
-
-        if feature_names[0].to_s == 'Restaurant 10'
-          OpenStudio::Extension.set_measure_argument(osw,
-                                                     'add_packaged_ice_storage', '__SKIP__', false)
-        end
-
-        if feature_names[0].to_s == 'Restaurant 12'
-          OpenStudio::Extension.set_measure_argument(osw,
-                                                     'add_packaged_ice_storage', '__SKIP__', false)
-        end
-
-        if feature_names[0].to_s == 'Restaurant 14'
-          OpenStudio::Extension.set_measure_argument(osw,
-                                                     'add_packaged_ice_storage', '__SKIP__', false)
-        end
-
-        if feature_names[0].to_s == 'Restaurant 15'
-          OpenStudio::Extension.set_measure_argument(osw,
-                                                     'add_packaged_ice_storage', '__SKIP__', false)
-        end
-
-        if feature_names[0].to_s == 'Office 1'
-          OpenStudio::Extension.set_measure_argument(osw,
-                                                     'add_central_ice_storage', '__SKIP__', false)
-          OpenStudio::Extension.set_measure_argument(osw, 'add_central_ice_storage', 'storage_capacity', 1200)
-        end
-
-        if feature_names[0].to_s == 'Hospital 1'
-          OpenStudio::Extension.set_measure_argument(osw,
-                                                     'add_central_ice_storage', '__SKIP__', false)
-          OpenStudio::Extension.set_measure_argument(osw, 'add_central_ice_storage', 'storage_capacity', 3000)
-        end
-
-        if feature_names[0].to_s == 'Hospital 2'
-          OpenStudio::Extension.set_measure_argument(osw,
-                                                     'add_central_ice_storage', '__SKIP__', false)
-          OpenStudio::Extension.set_measure_argument(osw, 'add_central_ice_storage', 'storage_capacity', 900)
-        end
-
-        if feature_names[0].to_s == 'Mixed use 2'
-          OpenStudio::Extension.set_measure_argument(osw,
-                                                     'add_central_ice_storage', '__SKIP__', false)
-          OpenStudio::Extension.set_measure_argument(osw, 'add_central_ice_storage', 'storage_capacity', 7000)
-        end
-
-        if feature_names[0].to_s == 'Restaurant 13'
-          OpenStudio::Extension.set_measure_argument(osw,
-                                                     'add_packaged_ice_storage', '__SKIP__', false)
-        end
-
-        if feature_names[0].to_s == 'Mall 1'
-          OpenStudio::Extension.set_measure_argument(osw,
-                                                     'add_central_ice_storage', '__SKIP__', false)
-          OpenStudio::Extension.set_measure_argument(osw, 'add_central_ice_storage', 'storage_capacity', 1500)
-        end
-
-        if feature_names[0].to_s == 'Hotel 1'
-          # PTAC coils must be explicitly excluded
-          ptac_coils = ['BUILDING STORY 10 THERMALZONE PTAC 1SPD DX AC CLG COIL 458KBTU/HR 9.5EER',
-                        'BUILDING STORY 4 THERMALZONE PTAC 1SPD DX AC CLG COIL 369KBTU/HR 9.5EER',
-                        'BUILDING STORY 5 THERMALZONE PTAC 1SPD DX AC CLG COIL 370KBTU/HR 9.5EER',
-                        'BUILDING STORY 6 THERMALZONE PTAC 1SPD DX AC CLG COIL 370KBTU/HR 9.5EER',
-                        'BUILDING STORY 7 THERMALZONE PTAC 1SPD DX AC CLG COIL 370KBTU/HR 9.5EER',
-                        'BUILDING STORY 8 THERMALZONE PTAC 1SPD DX AC CLG COIL 370KBTU/HR 9.5EER',
-                        'BUILDING STORY 9 THERMALZONE PTAC 1SPD DX AC CLG COIL 371KBTU/HR 9.5EER']
-
-          ptac_coils.each do |ptac|
-            OpenStudio::Extension.set_measure_argument(osw, 'add_packaged_ice_storage', ptac, false)
-          end
+          OpenStudio::Extension.set_measure_argument(osw, 'add_packaged_ice_storage', '__SKIP__', false)
+          OpenStudio::Extension.set_measure_argument(osw, 'add_packaged_ice_storage', 'size_mult', 1.0)
+          OpenStudio::Extension.set_measure_argument(osw, 'add_packaged_ice_storage', 'charge_start', 22)
+          OpenStudio::Extension.set_measure_argument(osw, 'add_packaged_ice_storage', 'charge_end', 7)
+          OpenStudio::Extension.set_measure_argument(osw, 'add_packaged_ice_storage', 'discharge_start', 12)
+          OpenStudio::Extension.set_measure_argument(osw, 'add_packaged_ice_storage', 'discharge_end', 18)
         end
 
         return osw
